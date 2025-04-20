0a7e6f81814fe73d6c8f9142743622dd52e660ad
Christos Ntokos <scur1354@int4.local.snellius.surf.nl>
2025-04-20 17:31:17 +0200

    final

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -8,8 +8,4 @@ python3 train_DeepLabV3_augmented.py \
     --num-workers 10 \
     --seed 42 \
     --backbone deeplabv3_resnet50 \
-    --quant_mode ptq \
-    --experiment-id "DL-RSN50-HIGH-AUGM-CE-QUANT" \
-
-
-#     --experiment-id "DL-RSN101-FOCAL_DICE_AUGM" \
+    --experiment-id "DL-RSN50-HIGH-AUGM-CE" \
